23d1e31eae2493e6102ffb3922e95b13e4884e7e
HanM.G <hanmoongoo@gmail.com>
2020-05-30 22:50:28 +0900

    bcrypt, User모델에 빠진 password추가...

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -9,6 +12,10 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         unique: 1
+    },
+    password:{
+        type: String,
+        minlength: 5
     },
     lastname: {
         type: String,
@@ -24,6 +31,25 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+//pre save전에 실행되게
+userSchema.pre('save',function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        // 비밀번호를 암호화시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+               // Store hash in your password DB.
+               if(err) return next(err)
+               user.password = hash
+               next()
+            })
+        })
+    }else{
+        next()
     }
 })
 
