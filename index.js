c8fd4a20dadb2d5a842acd953e2aec2737b06892
HanM.G <hanmoongoo@gmail.com>
2020-06-01 22:18:56 +0900

    token을 이용한 auth, logout

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const { User } = require('./models/User')
+const { auth } = require('./middleware/auth')
 const cookieParser = require('cookie-parser')
 
 const config = require('./config/key')
@@ -22,7 +23,7 @@ app.use(cookieParser())
 
 app.get('/',(req,res) => res.send('Hello World'))
 
-app.post('/register', (req,res) => {
+app.post('/api/users/register', (req,res) => {
     // 회원가입할때 필요한 정보들을 Client에서 가져오면
     // 그것들을 데이터베이스에 넣어준다.
 
@@ -38,7 +39,7 @@ app.post('/register', (req,res) => {
     })
 })
 
-app.post('/login',(req,res) => {
+app.post('/api/users/login',(req,res) => {
     // 요청된 이메일을 데이터베이스에서 찾는다.
     User.findOne({email : req.body.email}, (err,user) => {
         if(!user){
@@ -67,6 +68,35 @@ app.post('/login',(req,res) => {
         })
     })
 })
+
+// role 1 어드민 , role 2 특정부서 어드민
+// role 0 일반유저 role이 0이 아니면 관리자
+
+app.get('/api/users/auth', auth, (req,res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 ture라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname:req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+// 로그아웃
+app.get('/api/users/logout', auth, (req, res) =>{
+    // 유저를 찾아서 업데이트
+    User.findOneAndUpdate({_id:req.user._id},{token: ""}, (err, user)=>{
+        if(err) return res.json({success: false, err});
+        return res.status(200).send({
+            success:true
+        })
+    })
+})
+
 
 
 
